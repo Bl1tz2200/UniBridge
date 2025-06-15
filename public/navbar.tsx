66bc92b512ba14a7a13503ab0e6e58f422f5adb9
Bl1tz2200 <yaroslavkazan2008@gmail.com>
2025-06-16 01:02:04 +0300

    Added NavBar

---
+++ public/navbar.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState }  from "react";
+
+interface changingStyleParameter { // Creating class, that will be used to set CSS params dinamically 
+    switch: boolean,
+    param: {[key: string]: string}
+}
+
+
+export function NavBar() { // Returning navbar
+
+  // These will be shown only for mobile Users
+  const [dropdownMenu, setDropdownMenu] = useState<changingStyleParameter>({switch: true, param: {"moveToView": ""}})
+  const [burgerState, setBurgerState] = useState<changingStyleParameter>({switch: true, param: {"topLine": "bg-black", "middleLine": "", "bottomLine": "bg-black"}})
+
+
+  function openNavMenu(){ // Change state between opened navmenu or closed
+        if (burgerState.switch && dropdownMenu.switch){
+            setDropdownMenu({switch: !dropdownMenu.switch, param:{"moveToView": "translate-y-35"}})
+            setBurgerState({switch: !burgerState.switch, param:{"topLine": "bg-[#85c05d] rotate-45 translate-y-1", "middleLine": "hidden", "bottomLine": "bg-[#85c05d] rotate-135 -translate-y-1"}})
+        } else {
+            setDropdownMenu({switch: !dropdownMenu.switch, param:{"moveToView": ""}})
+            setBurgerState({switch: !burgerState.switch, param:{"topLine": "bg-black", "middleLine": "", "bottomLine": "bg-black"}})
+         }
+    }
+
+  return (
+    <nav className="sticky mt-0 top-0 w-[100%]">
+      <header className="relative flex  p-3 md:p-5 pt-5 justify-between items-center shadow-xl z-10 bg-white">
+        <section className="hidden sm:flex text-sm md:text-xl gap-5 justify-start">
+          <div><a href="/search" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Search</a></div>
+          <div><a href="/guidelines" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Guidelines</a></div>
+          <div><a href="/community" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Community</a></div>
+        </section>
+        <section className="w-3/12 text-2xl md:text-3xl mt-1 mb-1 text-center ">
+          <div><a href="/" className="font-(family-name:--font-rubik) hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">UniBridge</a></div>
+        </section>
+        <section className="hidden sm:flex w-3/12 text-sm md:text-xl gap-5 justify-end">
+          <div><a href="/mentor" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">AI-Mentor</a></div>
+          <div><a href="/profile" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Profile</a></div>
+        </section>
+        <section className="sm:hidden p-2 w-10">
+            <div className="group grid colomns-1 gap-1 items-center" onClick={openNavMenu}>
+              <span className={`${burgerState.param.topLine} w-10/12 h-1 duration-500 transition-transform rounded-full`}></span>
+              <span className={`${burgerState.param.middleLine} w-10/12 bg-black h-1 duration-500 transition-transform rounded-full`}></span>
+              <span className={`${burgerState.param.bottomLine} w-10/12 h-1 duration-500 transition-transform rounded-full`}></span>
+            </div>
+        </section>
+      </header>
+      <ul className={`${dropdownMenu.param.moveToView} -mt-35 sm:hidden z-0 grid columns-1 w-screen border-b-1 shadow-xl shadow-black/20 transition-transform duration-500 bg-white`}>
+            <li className="text-center text-l p-0.5"><a href="/search" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Search</a></li>
+            <li className="text-center text-l p-0.5"><a href="/guidelines" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Guidelines</a></li>
+            <li className="text-center text-l p-0.5"><a href="/community" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Community</a></li>
+            <li className="text-center text-l p-0.5"><a href="/mentor" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">AI-Mentor</a></li>
+            <li className="text-center text-l p-0.5"><a href="/profile" className="hover:text-[#85c05d] focus:text-[#85c05d] transition-colors duration-200">Profile</a></li>
+      </ul>
+    </nav>
+  );
+}
